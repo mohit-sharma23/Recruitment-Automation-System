4a17d1fe5cc9ed9267ff936a3879ad00f0b6cc48
mohit <mohitsharma28610@gmail.com>
2023-05-02 22:15:13 +0530

    done

--- AdminPanel/urls.py
+++ AdminPanel/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path , include
 from AdminPanel import views
 from django.conf import settings
@@ -16,13 +15,4 @@ urlpatterns = [
    
 
 
-
-=======
-from AdminPanel import views
-from django.urls import path , include
-from django.conf import settings
-from django.contrib import admin
-urlpatterns=[
-    path('',views.check,name='check'),
->>>>>>> b5d40e780e340922b1b12a474200ae8fc6ceef48
 ]
